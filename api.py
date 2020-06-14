1e86a34713176b92617c4ff7b671020ee1d61639
fung1 <foong@ya.ru>
2020-06-14 14:08:07 +0300

    More changes

--- api.py
+++ api.py
@@ -10,7 +10,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 # initialization
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'the quick brown fox jumps over the lazy dog'
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('MYSQL_URI')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('JAWSDB_URL')
 app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 
 # extensions
